98c13efc6622c4c5d9f21ef928e77cc894a11dc1
Emmanuel Oare <54752731+emmaoare@users.noreply.github.com>
2022-06-09 15:08:36 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,344 @@
+/*jshint jquery:true */
+
+$(document).ready(function($) {
+	"use strict";
+
+	/* global google: false */
+	/*jshint -W018 */
+
+	/*-------------------------------------------------*/
+	/* =  portfolio isotope
+	/*-------------------------------------------------*/
+
+	var winDow = $(window);
+		// Needed variables
+		var $container=$('.iso-call');
+		var $filter=$('.filter');
+
+		try{
+			$container.imagesLoaded( function(){
+				$container.trigger('resize');
+				$container.isotope({
+					filter:'*',
+					layoutMode:'masonry',
+					animationOptions:{
+						duration:750,
+						easing:'linear'
+					}
+				});
+
+				setTimeout(Resize, 1500);
+			});
+		} catch(err) {
+		}
+
+		winDow.on('resize', function(){
+			var selector = $filter.find('a.active').attr('data-filter');
+
+			try {
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+			}
+			return false;
+		});
+		
+		// Isotope Filter 
+		$filter.find('a').on('click', function(){
+			var selector = $(this).attr('data-filter');
+
+			try {
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+
+			}
+			return false;
+		});
+
+
+	var filterItemA	= $('.filter li a');
+
+		filterItemA.on('click', function(){
+			var $this = $(this);
+			if ( !$this.hasClass('active')) {
+				filterItemA.removeClass('active');
+				$this.addClass('active');
+			}
+		});
+
+	/* ---------------------------------------------------------------------- */
+	/*	magnific-popup
+	/* ---------------------------------------------------------------------- */
+
+	try {
+		// Example with multiple objects
+		$('.zoom').magnificPopup({
+			type: 'image',
+			gallery: {
+				enabled: true
+			}
+		});
+
+	} catch(err) {
+
+	}
+
+	/*-------------------------------------------------*/
+	/* =  Search animation
+	/*-------------------------------------------------*/
+	
+	var searchToggle = $('.open-search'),
+		inputAnime = $(".form-search"),
+		body = $('body');
+
+	searchToggle.on('click', function(event){
+		event.preventDefault();
+
+		if ( !inputAnime.hasClass('active') ) {
+			inputAnime.addClass('active');
+		} else {
+			inputAnime.removeClass('active');			
+		}
+	});
+
+	body.on('click', function(){
+		inputAnime.removeClass('active');
+	});
+
+	var elemBinds = $('.open-search, .form-search');
+	elemBinds.bind('click', function(e) {
+		e.stopPropagation();
+	});
+
+	/*-------------------------------------------------*/
+	/* =  count increment
+	/*-------------------------------------------------*/
+	try {
+		$('.statistic-post').appear(function() {
+			$('.timer').countTo({
+				speed: 4000,
+				refreshInterval: 60,
+				formatter: function (value, options) {
+					return value.toFixed(options.decimals);
+				}
+			});
+		});
+	} catch(err) {
+
+	}
+	
+	/*-------------------------------------------------*/
+	/* =  OWL carousell
+	/*-------------------------------------------------*/
+	try {
+		var owlWrap = $('.owl-wrapper');
+
+		if (owlWrap.length > 0) {
+
+			if (jQuery().owlCarousel) {
+				owlWrap.each(function(){
+
+					var carousel= $(this).find('.owl-carousel'),
+						dataNum = $(this).find('.owl-carousel').attr('data-num'),
+						dataNum2,
+						dataNum3;
+
+					if ( dataNum == 1 ) {
+						dataNum2 = 1;
+						dataNum3 = 1;
+					} else if ( dataNum == 2 ) {
+						dataNum2 = 2;
+						dataNum3 = dataNum - 1;
+					} else {
+						dataNum2 = dataNum - 1;
+						dataNum3 = dataNum - 2;
+					}
+
+					carousel.owlCarousel({
+						autoPlay: 10000,
+						navigation : true,
+						items : dataNum,
+						itemsDesktop : [1199,dataNum2],
+						itemsDesktopSmall : [991,dataNum3],
+						itemsTablet : [768, dataNum3],
+					});
+
+				});
+			}
+		}
+
+	} catch(err) {
+
+	}
+	
+	/* ---------------------------------------------------------------------- */
+	/*	Contact Map
+	/* ---------------------------------------------------------------------- */
+
+	try {
+	} catch(err) {
+
+	}
+
+	var toggleMapButton = $('a.toggle-map'),
+		MapSection = $('#map');
+
+		toggleMapButton.on('click', function(event){
+			event.preventDefault();
+
+			if ( toggleMapButton.hasClass('active') ) {
+				toggleMapButton.removeClass('active');
+				toggleMapButton.text('Show Map');
+			} else {
+				toggleMapButton.addClass('active');
+				toggleMapButton.text('Hide Map');
+			}
+
+			MapSection.slideToggle(function(){
+				mapfunction();
+			});
+
+		});
+	/*-------------------------------------------------*/
+	/* =  flexslider
+	/*-------------------------------------------------*/
+
+	try {
+
+		var SliderPost = $('.flexslider');
+
+		SliderPost.flexslider({
+			slideshowSpeed: 10000,
+			easing: "swing"
+		});
+	} catch(err) {
+
+	}
+
+	/* ---------------------------------------------------------------------- */
+	/*	Contact Form
+	/* ---------------------------------------------------------------------- */
+
+	var submitContact = $('#submit_contact'),
+		submitReservation = $('#submit_reservation'),
+		message = $('#msg');
+
+	submitContact.on('click', function(e){
+		e.preventDefault();
+
+		var $this = $(this);
+		
+		$.ajax({
+			type: "POST",
+			url: 'contact.php',
+			dataType: 'json',
+			cache: false,
+			data: $('#contact-form').serialize(),
+			success: function(data) {
+
+				if(data.info !== 'error'){
+					$this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
+					message.hide().removeClass('alert-success').removeClass('alert-danger').addClass('alert-success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				} else {
+					message.hide().removeClass('alert-success').removeClass('alert-danger').addClass('alert-danger').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				}
+			}
+		});
+	});
+
+	submitReservation.on('click', function(e){
+		e.preventDefault();
+
+		var $this = $(this);
+		
+		$.ajax({
+			type: "POST",
+			url: 'reservation.php',
+			dataType: 'json',
+			cache: false,
+			data: $('#reservation-form').serialize(),
+			success: function(data) {
+
+				if(data.info !== 'error'){
+					$this.parents('form').find('input[type=text]').filter(':visible').val('');
+					message.hide().removeClass('alert-success').removeClass('alert-danger').addClass('alert-success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				} else {
+					message.hide().removeClass('alert-success').removeClass('alert-danger').addClass('alert-danger').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				}
+			}
+		});
+	});
+
+	/* ---------------------------------------------------------------------- */
+	/*	Header animate after scroll
+	/* ---------------------------------------------------------------------- */
+
+	(function() {
+
+		var docElem = document.documentElement,
+			didScroll = false,
+			changeHeaderOn = 40;
+			document.querySelector( 'header, a.go-top' );
+		function init() {
+			window.addEventListener( 'scroll', function() {
+				if( !didScroll ) {
+					didScroll = true;
+					setTimeout( scrollPage, 100 );
+				}
+			}, false );
+		}
+		
+		function scrollPage() {
+			var sy = scrollY();
+			if ( sy >= changeHeaderOn ) {
+				$( 'header' ).addClass('active');
+				$( 'a.go-top' ).addClass('active');
+			}
+			else {
+				$( 'header' ).removeClass('active');
+				$( 'a.go-top' ).removeClass('active');
+			}
+			didScroll = false;
+		}
+		
+		function scrollY() {
+			return window.pageYOffset || docElem.scrollTop;
+		}
+		
+		init();
+		
+	})();
+
+});
+
+function Resize() {
+	$(window).trigger('resize');
+}
+
+function mapfunction() {
+	var fenway = [42.345573,-71.098326]; //Change a map coordinate here!
+	var markerPosition = [42.345573,-71.098326]; //Change a map marker here!
+	$('#map').gmap3({
+			center: fenway,
+			zoom: 13,
+			scrollwheel: false,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		})
+		.marker({
+			position: markerPosition,
+			icon: 'images/marker.png'
+	});
+}
